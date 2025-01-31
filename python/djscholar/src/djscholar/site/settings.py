d24536b0bfdc6d167d71eaf2f1bc598c15b97aea
Nate Smith <nsmith@archive.org>
2025-01-30 16:50:22 -0800

    connect to local socket db

--- python/djscholar/src/djscholar/site/settings.py
+++ python/djscholar/src/djscholar/site/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'djscholar.site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fatcat2',
+        'HOST': BASE_DIR / 'devdb/pgdata/sockets',
     }
 }
 

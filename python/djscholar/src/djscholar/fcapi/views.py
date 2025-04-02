89919e498b35eb88812e945944dcccf139de5b09
Nate Smith <nsmith@archive.org>
2025-04-02 11:52:07 -0700

    fill in container routes, mostly untested

--- python/djscholar/src/djscholar/fcapi/views.py
+++ python/djscholar/src/djscholar/fcapi/views.py
@@ -1,9 +1,12 @@
-from typing import List, Optional
+from typing import List
 
-from ninja import NinjaAPI, ModelSchema
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
+from ninja import NinjaAPI
+from ninja.orm import create_schema
 from ninja_apikey.security import APIKeyAuth
 
-import djscholar.fcapi.models as models
+from djscholar.fcapi.models import Container, Release
 
 v2api = NinjaAPI()
 # NB: uses X-API-Key header. use admin to create keys.
@@ -11,56 +14,85 @@ apiAuth = APIKeyAuth()
 
 # TODO ModelSchema for return types
 # TODO filter hidden things
-
-def lookup_entity(entiy_type: str, id_type: str, id_value: str) -> Optional[models.Entity]:
-    # TODO
-    return None
-
-def get_entity(entity_type: str, ident: str) -> Optional[models.Entity]:
-    # TODO
-    return None
-
-def delete_entity(entity_type: str, ident: str) -> Optional[models.Entity]:
-    return None
-
-# Container routes
+# TODO consider generalizing route implementations if it doesn't make signatures too hideous / doesn't break doc generation
+# TODO pagination
 
 COMMON_ENTITY_FIELDS = ["id", "created", "updated", "source", "extra"]
 
-class ContainerSchema(ModelSchema):
-    # TODO consider adding a release_count field
-    class Meta:
-        model = models.Container
-        fields = COMMON_ENTITY_FIELDS + ["name", "container_type", "publisher", "issnl",
-                                         "issne", "issnp", "wikidata_qid",]
+# In/Out schemas
+
+ContainerSchema = create_schema(Container,
+                                fields=COMMON_ENTITY_FIELDS\
+                                       + ["name", "container_type", "publisher", "issnl",
+                                          "issne", "issnp", "wikidata_qid",])
+
+ReleaseSchema = create_schema(Release,
+                              fields=COMMON_ENTITY_FIELDS\
+                                      + ["work", "container", "title", "original_title",
+                                         "subtitle", "release_type", "release_stage",
+                                         "release_date", "release_year", "volume", "issue",
+                                         "pages", "number", "version", "publisher", "language",
+                                         "license_slug", "withdrawn_status", "refs",])
+
+
+# Container routes
 
 @v2api.get("/container/lookup")
-def container_lookup(request, id_type: str, id_value: str) -> Optional[ContainerSchema]:
-    cs = models.Container.objects.filter(**{id_type: id_value})
+def container_lookup(request, id_type: str, id_value: str) -> ContainerSchema:
+    cs = Container.objects.filter(**{id_type: id_value})
     if len(cs) == 0:
-        return None
+        raise Http404(f"no container found with {id_type} of {id_value}")
     return ContainerSchema.from_orm(cs[0])
 
 @v2api.get("/container/{ident}")
-def container_get(request, ident: str) -> Optional[models.Container]:
-    # TODO
-    return {}
+def container_get(request, ident: str) -> ContainerSchema:
+    # TODO handle legacy idents
+    return ContainerSchema.from_orm(get_object_or_404(Container, id=ident))
 
 @v2api.get("/container/{ident}/releases")
-def container_releases(request, ident: str) -> List[models.Release]:
-    # TODO
-    return []
+def container_releases(request, ident: str) -> List[ReleaseSchema]:
+    # TODO handle legacy idents
+    return [ReleaseSchema.from_orm(r) for r in Release.objects.filter(container__id=ident)]
 
-@v2api.delete("/container", auth=apiAuth)
-def container_delete(ident: str) -> Optional[models.Container]:
-    # TODO
-    return None
+@v2api.delete("/container/{ident}", auth=apiAuth)
+def container_delete(request, ident: str) -> ContainerSchema:
+    # TODO handle legacy idents
+    c = get_object_or_404(Container, id=ident)
+    c.delete()
+    return ContainerSchema.from_orm(c)
 
-# TODO decide on patch updates vs overwrites (starting with patch updates)
 @v2api.post("/container", auth=apiAuth)
-def container_update(ident: str) -> Optional[models.Container]:
-    # TODO
-    return None
+def container_create(request, container_in: ContainerSchema) -> HttpResponse:
+    c = Container()
+    for attr, value in container_in.dict().items():
+        setattr(c, attr, value)
+    c.save()
+
+    return v2api.create_response(request, "container created", status=201)
+
+@v2api.put("/container", auth=apiAuth)
+def container_update(request, container_in: ContainerSchema) -> HttpResponse:
+    """
+    Replace a container entity wholesale. Must specify entire content of entity; not a patch operation.
+    """
+    in_dict = container_in.dict()
+    c = get_object_or_404(Container, id=in_dict["id"])
+    for attr, value in in_dict.items():
+        setattr(c, attr, value)
+    c.save()
+    return v2api.create_response(request, "container created", status=200)
+
+@v2api.post("/containers", auth=apiAuth)
+def container_batch_create(request, containers_in: List[ContainerSchema]) -> HttpResponse:
+    cs: List[Container] = []
+    for cin in containers_in:
+        c = Container()
+        for attr, value in cin.dict():
+            setattr(c, attr, value)
+        cs.append(c)
+    Container.objects.bulk_create(cs)
+
+    return v2api.create_response(request, "containers created", status=201)
 
 # Release routes
 

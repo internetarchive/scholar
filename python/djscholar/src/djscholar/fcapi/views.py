7b79da681d8c7f300bca7423862c97417400b842
Nate Smith <nsmith@archive.org>
2025-04-18 18:01:42 -0500

    use alias for model imports

--- python/djscholar/src/djscholar/fcapi/views.py
+++ python/djscholar/src/djscholar/fcapi/views.py
@@ -7,8 +7,8 @@ from ninja import NinjaAPI, Query, Schema, ModelSchema
 from ninja.orm import create_schema
 from ninja_apikey.security import APIKeyAuth
 
+import djscholar.fcapi.models as m
 from djscholar.fcapi.fcid import fcid2uuid
-from djscholar.fcapi.models import Container, Creator, File, Release, ReleaseContrib, RELEASE_EXT_ID_TYPES, Work
 
 v2api = NinjaAPI()
 # NB: uses X-API-Key header. use admin to create keys.
@@ -19,7 +19,7 @@ apiAuth = APIKeyAuth()
 # signatures too hideous / doesn't break doc generation
 # TODO pagination
 # TODO support nested containers and works (and possibly other types) during creation/update; possibly getting
-# TODO add legacy_ident lookup to all entity lookup routes
+# TODO add get_release_webcaptures
 
 COMMON_ENTITY_FIELDS = ["id", "created", "updated", "source", "extra"]
 
@@ -41,7 +41,7 @@ class FileLookup(Schema):
 
 
 class ReleaseLookup(Schema):
-    id_type: Literal[*[t[0] for t in RELEASE_EXT_ID_TYPES] + ["legacy_ident"]]
+    id_type: Literal[*[t[0] for t in m.RELEASE_EXT_ID_TYPES] + ["legacy_ident"]]
     id_value: str
 
 
@@ -50,7 +50,7 @@ class WorkLookup(Schema):
     id_value: str
 
 
-ContainerSchema = create_schema(Container,
+ContainerSchema = create_schema(m.Container,
                                 fields=COMMON_ENTITY_FIELDS\
                                        + ["name", "container_type", "publisher", "issnl",
                                           "issne", "issnp", "wikidata_qid",])
@@ -59,18 +59,20 @@ ContainerSchema = create_schema(Container,
 class ReleaseSchema(ModelSchema):
     work_id: uuid.UUID
     container_id: Optional[uuid.UUID]
+    # TODO support a map of external IDs to use when creating/updating
+    # TODO support a map of contribs to use when creating/updating
 
     class Meta:
-        model = Release
+        model = m.Release
         fields = COMMON_ENTITY_FIELDS + ["title", "original_title", "subtitle", "release_type",
                                          "release_stage", "release_date", "release_year",
                                          "volume", "issue", "pages", "number", "version",
                                          "publisher", "language", "license_slug",
                                          "withdrawn_status", "refs",]
 
-WorkSchema = create_schema(Work, fields=COMMON_ENTITY_FIELDS)
+WorkSchema = create_schema(m.Work, fields=COMMON_ENTITY_FIELDS)
 
-CreatorSchema = create_schema(Creator, fields=COMMON_ENTITY_FIELDS+["display_name", "given_name",
+CreatorSchema = create_schema(m.Creator, fields=COMMON_ENTITY_FIELDS+["display_name", "given_name",
                                                                     "surname", "orcid"])
 
 class ReleaseContribSchema(ModelSchema):
@@ -93,9 +95,9 @@ def lookup_container(request, lookup: Query[ContainerLookup]) -> ContainerSchema
     the ID, an arbitrary one is returned."""
     if lookup.id_type == "legacy_ident":
         ident = fcid2uuid(lookup.id_value)
-        return ContainerSchema.from_orm(get_object_or_404(Container, id=ident))
+        return ContainerSchema.from_orm(get_object_or_404(m.Container, id=ident))
 
-    cs = Container.objects.filter(**{lookup.id_type: lookup.id_value})
+    cs = m.Container.objects.filter(**{lookup.id_type: lookup.id_value})
     if len(cs) == 0:
         raise Http404(f"no container found with {lookup.id_type} of {lookup.id_value}")
     return ContainerSchema.from_orm(cs[0])
@@ -103,31 +105,31 @@ def lookup_container(request, lookup: Query[ContainerLookup]) -> ContainerSchema
 @v2api.get("/container/{ident}")
 def get_container(request, ident: str) -> ContainerSchema:
     """Get a single container by its ID."""
-    return ContainerSchema.from_orm(get_object_or_404(Container, id=ident))
+    return ContainerSchema.from_orm(get_object_or_404(m.Container, id=ident))
 
 @v2api.get("/container/{ident}/releases")
 def get_container_releases(request, ident: str) -> List[ReleaseSchema]:
     """Get all releases for a given container ID."""
     # TODO paginate
-    return [ReleaseSchema.from_orm(r) for r in Release.objects.filter(container__id=ident)]
+    return [ReleaseSchema.from_orm(r) for r in m.Release.objects.filter(container__id=ident)]
 
 
 @v2api.post("/container", auth=apiAuth)
 def create_container(request, container_in: ContainerSchema) -> HttpResponse:
     """Create a new container."""
-    cs = Container.objects.filter(id=container_in.id)
+    cs = m.Container.objects.filter(id=container_in.id)
     if len(cs) != 0:
         return v2api.create_response(request,
                                      f"container with id {container_in.id} already exists",
                                      status=400)
-    Container(**container_in.dict()).save()
+    m.Container(**container_in.dict()).save()
     return v2api.create_response(request, "container created", status=201)
 
 @v2api.post("/containers", auth=apiAuth)
 def bulk_create_containers(request, containers_in: List[ContainerSchema]) -> HttpResponse:
     """Bulk create a list of containers. Functionally equivalent to calling
     POST /container repeatedly."""
-    Container.objects.bulk_create([Container(**cin.dict()) for cin in containers_in])
+    m.Container.objects.bulk_create([m.Container(**cin.dict()) for cin in containers_in])
     return v2api.create_response(request, "containers created", status=201)
 
 @v2api.put("/container", auth=apiAuth)
@@ -138,12 +140,12 @@ def update_container(request, container_in: ContainerSchema) -> HttpResponse:
     201 if a new release was created; 200 otherwise.
     """
     code = 200
-    es = Container.objects.filter(id=container_in.id)
+    es = m.Container.objects.filter(id=container_in.id)
     entity = None
 
     if len(es) == 0:
         code = 201
-        entity = Container(**container_in.dict())
+        entity = m.Container(**container_in.dict())
     else:
         entity = es[0]
         for attr, value in container_in.dict().items():
@@ -156,7 +158,7 @@ def update_container(request, container_in: ContainerSchema) -> HttpResponse:
 @v2api.delete("/container/{ident}", auth=apiAuth)
 def delete_container(request, ident: str) -> ContainerSchema:
     """Delete the container with a given ID."""
-    c = get_object_or_404(Container, id=ident)
+    c = get_object_or_404(m.Container, id=ident)
     out = ContainerSchema.from_orm(c)
     c.delete()
     return out
@@ -169,9 +171,9 @@ def lookup_release(request, lookup: Query[ReleaseLookup]) -> ReleaseSchema:
     ID, an arbitrary one is returned."""
     if lookup.id_type == "legacy_ident":
         ident = fcid2uuid(lookup.id_value)
-        return ReleaseSchema.from_orm(get_object_or_404(Release, id=ident))
+        return ReleaseSchema.from_orm(get_object_or_404(m.Release, id=ident))
 
-    rs = Release.objects.filter(**{
+    rs = m.Release.objects.filter(**{
         "releaseextid__id_type": lookup.id_type,
         "releaseextid__id_value": lookup.id_value})
     if len(rs) == 0:
@@ -181,24 +183,24 @@ def lookup_release(request, lookup: Query[ReleaseLookup]) -> ReleaseSchema:
 @v2api.get("/release/{ident}")
 def get_release(request, ident: str) -> ReleaseSchema:
     """Get a single release by its ID."""
-    return ReleaseSchema.from_orm(get_object_or_404(Release, id=ident))
+    return ReleaseSchema.from_orm(get_object_or_404(m.Release, id=ident))
 
 @v2api.post("/release", auth=apiAuth)
 def create_release(request, release_in: ReleaseSchema) -> HttpResponse:
     """Create a new release."""
     # TODO releases without works should have works created automagically
-    rs = Release.objects.filter(id=release_in.id)
+    rs = m.Release.objects.filter(id=release_in.id)
     if len(rs) != 0:
         return v2api.create_response(request,
                                      f"release with id {release_in.id} already exists",
                                      status=400)
-    Release(**release_in.dict()).save()
+    m.Release(**release_in.dict()).save()
     return v2api.create_response(request, "release created", status=201)
 
 @v2api.get("/release/{ident}/container")
 def get_release_container(request, ident: str) -> ContainerSchema:
     """Get a release's container (ie, journal)"""
-    cs = Container.objects.filter(release__id=ident)
+    cs = m.Container.objects.filter(release__id=ident)
     if len(cs) == 0:
         raise Http404(f"release {ident} has no associated container")
     return ContainerSchema.from_orm(cs[0])
@@ -206,13 +208,13 @@ def get_release_container(request, ident: str) -> ContainerSchema:
 @v2api.get("/release/{ident}/work")
 def get_release_work(request, ident: str) -> WorkSchema:
     """Get a the work that represents the platonic version of this release."""
-    ws = Container.objects.filter(release__id=ident)
+    ws = m.Container.objects.filter(release__id=ident)
     # do not need to check length; work_id is required in schema
     return WorkSchema.from_orm(ws[0])
 
 @v2api.get("/release/{ident}/files")
 def get_release_files(request, ident: str) -> List[FileSchema]:
-    return [FileSchema.from_orm(e) for e in File.objects.filter(releasefile__release_id=ident)]
+    return [FileSchema.from_orm(e) for e in m.File.objects.filter(releasefile__release_id=ident)]
 
 @v2api.get("/release/{ident}/contribs")
 def get_release_contribs(request, ident: str) -> List[CreatorSchema]:
@@ -221,12 +223,12 @@ def get_release_contribs(request, ident: str) -> List[CreatorSchema]:
     richer information about a contribution (eg, orcid); many contributions
     will just be raw names pulled from a paper's author list, however."""
     return [ReleaseContribSchema.from_orm(rc)
-            for rc in ReleaseContrib.objects.filter(release_id=ident)]
+            for rc in m.ReleaseContrib.objects.filter(release_id=ident)]
 
 @v2api.delete("/release/{ident}", auth=apiAuth)
 def delete_release(request, ident: str) -> ReleaseSchema:
     """Delete the release with a given ID."""
-    r = get_object_or_404(Release, id=ident)
+    r = get_object_or_404(m.Release, id=ident)
     out = ReleaseSchema.from_orm(r)
     r.delete()
     return out
@@ -239,12 +241,12 @@ def update_release(request, release_in: ReleaseSchema) -> HttpResponse:
     201 if a new release was created; 200 otherwise.
     """
     code = 200
-    es = Release.objects.filter(id=release_in.id)
+    es = m.Release.objects.filter(id=release_in.id)
     entity = None
 
     if len(es) == 0:
         code = 201
-        entity = Release(**release_in.dict())
+        entity = m.Release(**release_in.dict())
     else:
         entity = es[0]
         for attr, value in release_in.dict().items():
@@ -259,7 +261,7 @@ def bulk_create_releases(request, releases_in: List[ReleaseSchema]) -> HttpRespo
     """Bulk create a list of releases. Functionally equivalent to calling
     POST /release repeatedly."""
     # TODO releases without works should have works created automagically
-    Release.objects.bulk_create([Release(**rin.dict()) for rin in releases_in])
+    m.Release.objects.bulk_create([m.Release(**rin.dict()) for rin in releases_in])
     return v2api.create_response(request, "releases created", status=201)
 
 
@@ -270,24 +272,24 @@ def lookup_work(request, lookup: Query[WorkLookup]) -> WorkSchema:
     """Lookup a work by metadata other than its UUID"""
     if lookup.id_type == "legacy_ident":
         ident = fcid2uuid(lookup.id_value)
-        return WorkSchema.from_orm(get_object_or_404(Work, id=ident))
+        return WorkSchema.from_orm(get_object_or_404(m.Work, id=ident))
     else:
         raise NotImplementedError()
 
 @v2api.get("/work/{ident}")
 def get_work(request, ident: str) -> WorkSchema:
     """Get a work (collection of releases) by its ID"""
-    return WorkSchema.from_orm(get_object_or_404(Work, id=ident))
+    return WorkSchema.from_orm(get_object_or_404(m.Work, id=ident))
 
 @v2api.get("/work/{ident}/releases")
 def get_work_releases(request, ident: str) -> List[ReleaseSchema]:
     """Get all releases associated with a work's ID"""
-    return [ReleaseSchema.from_orm(r) for r in Release.objects.filter(work_id=ident)]
+    return [ReleaseSchema.from_orm(r) for r in m.Release.objects.filter(work_id=ident)]
 
 @v2api.delete("/work/{ident}", auth=apiAuth)
 def delete_work(request, ident: str) -> WorkSchema:
     """Delete a work by its ID"""
-    entity = get_object_or_404(Work, id=ident)
+    entity = get_object_or_404(m.Work, id=ident)
     out = WorkSchema.from_orm(entity)
     entity.delete()
     return out
@@ -296,12 +298,12 @@ def delete_work(request, ident: str) -> WorkSchema:
 def update_work(request, work_in: WorkSchema) -> HttpResponse:
     """Replace a work record wholesale."""
     code = 200
-    es = Work.objects.filter(id=work_in.id)
+    es = m.Work.objects.filter(id=work_in.id)
     entity = None
 
     if len(es) == 0:
         code = 201
-        entity = Work(**work_in.dict())
+        entity = m.Work(**work_in.dict())
     else:
         entity = es[0]
         for attr, value in work_in.dict().items():
@@ -321,9 +323,9 @@ def lookup_creator(request, lookup: Query[CreatorLookup]) -> CreatorSchema:
     creators match the ID, an arbitrary one is returned."""
     if lookup.id_type == "legacy_ident":
         ident = fcid2uuid(lookup.id_value)
-        return CreatorSchema.from_orm(get_object_or_404(Creator, id=ident))
+        return CreatorSchema.from_orm(get_object_or_404(m.Creator, id=ident))
 
-    es = Creator.objects.filter(**{lookup.id_type: lookup.id_value})
+    es = m.Creator.objects.filter(**{lookup.id_type: lookup.id_value})
     if len(es) == 0:
         raise Http404(f"no creator found with {lookup.id_type} of {lookup.id_value}")
     return CreatorSchema.from_orm(es[0])
@@ -334,34 +336,34 @@ def get_creator_releases(request, ident: str) -> List[ReleaseSchema]:
     releases, their authors exist only as raw contribs and do not have creator
     records."""
     # TODO paginate
-    return [ReleaseSchema.from_orm(r) for r in Release.objects.filter(
+    return [ReleaseSchema.from_orm(r) for r in m.Release.objects.filter(
         releasecontrib__creator_id=ident)]
 
 @v2api.get("/creator/{ident}")
 def get_creator(request, ident: str) -> CreatorSchema:
-    return CreatorSchema.from_orm(get_object_or_404(Creator, id=ident))
+    return CreatorSchema.from_orm(get_object_or_404(m.Creator, id=ident))
 
 @v2api.post("/creator", auth=apiAuth)
 def create_creator(request, creator_in: CreatorSchema) -> HttpResponse:
     """Create a new creator."""
-    es = Creator.objects.filter(id=creator_in.id)
+    es = m.Creator.objects.filter(id=creator_in.id)
     if len(es) != 0:
         return v2api.create_response(request,
                                      f"creator with id {creator_in.id} already exists",
                                      status=400)
-    Creator(**creator_in.dict()).save()
+    m.Creator(**creator_in.dict()).save()
     return v2api.create_response(request, "creator created", status=201)
 
 @v2api.put("/creator", auth=apiAuth)
 def update_creator(request, creator_in: CreatorSchema) -> HttpResponse:
     """Replace a creator record wholesale."""
     code = 200
-    es = Creator.objects.filter(id=creator_in.id)
+    es = m.Creator.objects.filter(id=creator_in.id)
     entity = None
 
     if len(es) == 0:
         code = 201
-        entity = Creator(**creator_in.dict())
+        entity = m.Creator(**creator_in.dict())
     else:
         entity = es[0]
         for attr, value in creator_in.dict().items():
@@ -373,13 +375,13 @@ def update_creator(request, creator_in: CreatorSchema) -> HttpResponse:
 
 @v2api.post("/creators", auth=apiAuth)
 def bulk_create_creators(request, creators_in: List[CreatorSchema]) -> HttpResponse:
-    Creator.objects.bulk_create([Creator(**cin.dict()) for cin in creators_in])
+    m.Creator.objects.bulk_create([m.Creator(**cin.dict()) for cin in creators_in])
     return v2api.create_response(request, "creators created", status=201)
 
 @v2api.delete("/creator/{ident}", auth=apiAuth)
 def delete_creator(request, ident: str) -> HttpResponse:
     """Delete a creator record. Note: does not delete associated releases."""
-    entity = get_object_or_404(Creator, id=ident)
+    entity = get_object_or_404(m.Creator, id=ident)
     out = CreatorSchema.from_orm(entity)
     entity.delete()
     return out
@@ -393,9 +395,9 @@ def lookup_file(request, lookup: Query[FileLookup]) -> FileSchema:
     """Look up a file by checksum."""
     if lookup.id_type == "legacy_ident":
         ident = fcid2uuid(lookup.id_value)
-        return FileSchema.from_orm(get_object_or_404(File, id=ident))
+        return FileSchema.from_orm(get_object_or_404(m.File, id=ident))
 
-    es = File.objects.filter(**{lookup.id_type: lookup.id_value})
+    es = m.File.objects.filter(**{lookup.id_type: lookup.id_value})
     if len(es) == 0:
         raise Http404(f"no file found with {lookup.id_type} of {lookup.id_value}")
     return FileSchema.from_orm(es[0])
@@ -404,36 +406,36 @@ def lookup_file(request, lookup: Query[FileLookup]) -> FileSchema:
 def get_file_releases(request, ident: str) -> List[ReleaseSchema]:
     """Get all the releases associated with a given file."""
     # TODO paginate
-    return [ReleaseSchema.from_orm(r) for r in Release.objects.filter(
+    return [ReleaseSchema.from_orm(r) for r in m.Release.objects.filter(
         releasefile__file_id=ident)]
 
 @v2api.get("/file/{ident}")
 def get_file(request, ident: str) -> FileSchema:
-    return FileSchema.from_orm(get_object_or_404(File, id=ident))
+    return FileSchema.from_orm(get_object_or_404(m.File, id=ident))
 
 @v2api.post("/file", auth=apiAuth)
 def create_file(request, file_in: FileSchema) -> HttpResponse:
     """Create a new file. Note that file contents do not live in this database;
     we only track checksums and metadata here. File contents, if stored at all,
     live in blob storage elsewhere."""
-    es = File.objects.filter(id=file_in.id)
+    es = m.File.objects.filter(id=file_in.id)
     if len(es) != 0:
         return v2api.create_response(request,
                                      f"file with id {file_in.id} already exists",
                                      status=400)
-    File(**file_in.dict()).save()
+    m.File(**file_in.dict()).save()
     return v2api.create_response(request, "file created", status=201)
 
 @v2api.put("/file", auth=apiAuth)
 def update_file(request, file_in: FileSchema) -> HttpResponse:
     """Replace a file record wholesale."""
     code = 200
-    es = File.objects.filter(id=file_in.id)
+    es = m.File.objects.filter(id=file_in.id)
     entity = None
 
     if len(es) == 0:
         code = 201
-        entity = File(**file_in.dict())
+        entity = m.File(**file_in.dict())
     else:
         entity = es[0]
         for attr, value in file_in.dict().items():
@@ -445,14 +447,14 @@ def update_file(request, file_in: FileSchema) -> HttpResponse:
 
 @v2api.post("/files", auth=apiAuth)
 def bulk_create_files(request, files_in: List[FileSchema]) -> HttpResponse:
-    File.objects.bulk_create([File(**cin.dict()) for cin in files_in])
+    m.File.objects.bulk_create([m.File(**cin.dict()) for cin in files_in])
     return v2api.create_response(request, "files created", status=201)
 
 @v2api.delete("/file/{ident}", auth=apiAuth)
 def delete_file(request, ident: str) -> HttpResponse:
     """Delete a file record. Does not delete associated releases. Actual
     file contents will continue to live in blob storage."""
-    entity = get_object_or_404(File, id=ident)
+    entity = get_object_or_404(m.File, id=ident)
     out = FileSchema.from_orm(entity)
     entity.delete()
     return out

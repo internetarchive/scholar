5ee723b6fd9de76b937c29c92d8b5fb2b10ddb3a
Nate Smith <nsmith@archive.org>
2025-05-07 17:25:16 -0700

    WIP reporting

--- go/report/main.go
+++ go/report/main.go
@@ -5,11 +5,15 @@ The purpose of this program is to produce a simple, rough report on the past 24 
 */
 
 import (
+	"bytes"
 	_ "embed"
 	"fmt"
 	"html/template"
+	"net/http"
 	"os"
 )
+
+const esURL = "https://scholar.archive.org/_es/scholar_fulltext/_count"
 
 //go:embed report.tmpl.html
 var reportTmpl string
@@ -29,14 +33,32 @@ type reportCtx struct {
 }
 
 func _main() error {
-
 	tmpl, err := template.New("report").Parse(reportTmpl)
 	if err != nil {
 		return fmt.Errorf("template parsing failed: %w", err)
 	}
 	tmplCtx := reportCtx{PDFMissReasons: []PDFMissReason{}}
-	// TODO consume https://scholar.archive.org/fatcat/stats.json
+	client := &http.Client{}
+	body := bytes.NewBufferString(`
+{"query": {
+      "bool": {"must": [{"term": { "access.access_type": "wayback"}},
+			                  {"term": {"access.mimetype": "application/pdf"}}]}}
+}`)
+	esReq, err := http.NewRequest(http.MethodGet, esURL, body)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	esReq.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(esReq)
+	if err != nil {
+		return fmt.Errorf("failed to talk to elasticsearch: %w", err)
+	}
+
+	// TODO
+	fmt.Println(resp)
+
 	// TODO consume https://scholar.archive.org/_es
+	// TODO consume https://scholar.archive.org/fatcat/stats.json
 	// TODO consume http://wbgrp-svc506.us.archive.org:3030/rpc
 	err = tmpl.Execute(os.Stdout, tmplCtx)
 	if err != nil {

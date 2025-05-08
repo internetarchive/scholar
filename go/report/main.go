ef4919d7667d5b822ab5dbc7555b1dc63d1a71a2
Nate Smith <nsmith@archive.org>
2025-05-08 12:13:36 -0700

    parse ES response

--- go/report/main.go
+++ go/report/main.go
@@ -2,13 +2,22 @@ package main
 
 /*
 The purpose of this program is to produce a simple, rough report on the past 24 hours of scholar activity.
+
+thoughts regarding stats over time: add columns for the past week of stats.
+keep stats serialized as flat files and just grab the last N where N >=6 to
+combine with the current day.
+
+can have a check that shreds files when there's more than some limit. they will be tiny files so the limit can be stupid high--maybe 365*3.
+
 */
 
 import (
 	"bytes"
 	_ "embed"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
 )
@@ -32,32 +41,60 @@ type reportCtx struct {
 	PDFMissReasons       []PDFMissReason
 }
 
-func _main() error {
-	tmpl, err := template.New("report").Parse(reportTmpl)
-	if err != nil {
-		return fmt.Errorf("template parsing failed: %w", err)
-	}
-	tmplCtx := reportCtx{PDFMissReasons: []PDFMissReason{}}
-	client := &http.Client{}
+func getFulltextPDFCount(c *http.Client) (int, error) {
 	body := bytes.NewBufferString(`
 {"query": {
       "bool": {"must": [{"term": { "access.access_type": "wayback"}},
 			                  {"term": {"access.mimetype": "application/pdf"}}]}}
 }`)
-	esReq, err := http.NewRequest(http.MethodGet, esURL, body)
+	req, err := http.NewRequest(http.MethodGet, esURL, body)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return -1, fmt.Errorf("failed to create request: %w", err)
 	}
-	esReq.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(esReq)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := c.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to talk to elasticsearch: %w", err)
+		return -1, fmt.Errorf("failed to talk to elasticsearch: %w", err)
 	}
 
-	// TODO
-	fmt.Println(resp)
+	if resp.StatusCode != http.StatusOK {
+		return -1, fmt.Errorf("got %d from ES", resp.StatusCode)
+	}
 
-	// TODO consume https://scholar.archive.org/_es
+	esCount := struct {
+		Count int
+	}{}
+
+	rbody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return -1, fmt.Errorf("could not read ES response: %w", err)
+	}
+
+	err = json.Unmarshal(rbody, &esCount)
+
+	if err != nil {
+		return -1, fmt.Errorf("could not parse ES response: %w", err)
+	}
+
+	return esCount.Count, nil
+}
+
+func _main() error {
+	tmpl, err := template.New("report").Parse(reportTmpl)
+	if err != nil {
+		return fmt.Errorf("template parsing failed: %w", err)
+	}
+	client := &http.Client{}
+
+	esCount, err := getFulltextPDFCount(client)
+	if err != nil {
+		return fmt.Errorf("failed to get fulltext PDF count: %w", err)
+	}
+
+	tmplCtx := reportCtx{
+		FulltextPDFCount: esCount,
+	}
+
 	// TODO consume https://scholar.archive.org/fatcat/stats.json
 	// TODO consume http://wbgrp-svc506.us.archive.org:3030/rpc
 	err = tmpl.Execute(os.Stdout, tmplCtx)

407863ad0645fdf4a079c925d878d7afa776805a
nate smith <nsmith@archive.org>
2025-09-22 12:22:19 -0700

    [trawler] add issn package

---
+++ trawler/issn/issn.go
@@ -0,0 +1,42 @@
+package issn
+
+import (
+	"embed"
+	"encoding/csv"
+	"errors"
+	"io"
+)
+
+//go:embed 2025.issn2issnl.tsv
+var ff embed.FS
+
+var issn2issnl map[string]string
+
+func init() {
+	f, err := ff.Open("2025.issn2issnl.tsv")
+	if err != nil {
+		panic(err)
+	}
+
+	r := csv.NewReader(f)
+	r.Comma = '\t'
+
+	for {
+		record, err := r.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		issn2issnl[record[0]] = record[1]
+	}
+}
+
+func ISSN2ISSNL(i string) string {
+	v, ok := issn2issnl[i]
+	if !ok {
+		return ""
+	}
+	return v
+}

b26ff948ab6ef0f90191e5856e20e6be6e9769c4
nate smith <nsmith@archive.org>
2026-03-11 17:06:59 -0700

    [djscholar] add initial urls and stub views for ftsearch

--- djscholar/ftsearch/views.py
+++ djscholar/ftsearch/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def webhealth(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def health(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def searchhealth(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def help(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def permalink(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def search(request: HttpRequest) -> HttpResponse:
+    raise NotImplementedError
+
+
+def work(request: HttpRequest, work_ident: str) -> HttpResponse:
+    raise NotImplementedError
+
+
+def access_redirect_wayback(request: HttpRequest, work_ident: str, url: str) -> HttpResponse:
+    raise NotImplementedError
+
+
+def access_redirect_ia_file(request: HttpRequest, work_ident: str, item: str, file_path: str) -> HttpResponse:
+    raise NotImplementedError
--- djscholar/src/djscholar/site/urls.py
+++ djscholar/src/djscholar/site/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from djscholar.fcapi.views import v2api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/fatcat/v2/", v2api.urls),
+    path("", include("ftsearch.urls")),
 ]

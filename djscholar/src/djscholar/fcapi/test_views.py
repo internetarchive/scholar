c36f2dfc74ad6ccf3179eb18f5216e1dd662165b
nate smith <nsmith@archive.org>
2026-03-18 18:07:41 -0700

    [djscholar] fix container test

--- djscholar/src/djscholar/fcapi/test_views.py
+++ djscholar/src/djscholar/fcapi/test_views.py
@@ -819,7 +819,7 @@ class TestContainerRoutes(EntityCRUDTestCase):
         self.assertEqual(len(cs), 1)
 
         response = client.post(self.create, data=data, headers=self.auth_headers)
-        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
 
     def test_bulk_create(self):
         cs = [v.ContainerSchema.from_orm(ContainerFactory.build()) for _ in range(100)]

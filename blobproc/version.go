2aed1f6686af17979b058d7679b58340fd0197ec
nate smith <nsmith@archive.org>
2026-06-01 14:51:40 -0700

    [ci] make blobproc build consistent with others, fix dlopen issue

--- blobproc/version.go
+++ blobproc/version.go
@@ -1,4 +1,5 @@
 package blobproc
 
-// Version of library and cli tools.
-const Version = "0.4.0"
+// Version of library and cli tools. Set at build time from the git tag via
+// -ldflags "-X .../blobproc.Version=<version>" (see Makefile); "dev" otherwise.
+var Version = "dev"
